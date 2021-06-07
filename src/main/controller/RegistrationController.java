c06e7d1d9b61bb458233bd03e3f7587553fa31fa
Oliver Hale <s3781403@student.rmit.edu.au>
2021-06-07 20:11:07 +1000

    test

--- src/main/controller/RegistrationController.java
+++ src/main/controller/RegistrationController.java
@@ -1,4 +1,76 @@
 package main.controller;
 
-public class RegistrationController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import main.model.RegistrationModel;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class RegistrationController implements Initializable {
+
+    public RegistrationModel registrationModel = new RegistrationModel();
+    public Button registerButton;
+
+    //Creates variables from the fxml registration file
+    @FXML
+    private TextField txtEmployeeID, txtFirstName, txtLastname, txtUsername;
+    @FXML
+    private TextField txtSecQuestion, txtSecAnswer, txtFirstPassword, txtSecondPassword;
+    @FXML
+    private Label dbStatus;
+    @FXML
+    private Label passwordMatch;
+    @FXML
+    private RadioButton staffRole;
+
+    private String tempRole = "staff";
+
+    //Check if database is connected
+        @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        if (registrationModel.isDbConnected()){
+            dbStatus.setText("Connected");
+        } else {
+            dbStatus.setText("Not connected");
+        }
+    }
+
+    //Where the logic goes to determine whether this is a successful registration or not
+    public void Register(ActionEvent actionEvent) throws SQLException {
+
+            if(checkPassword()) {
+                try {
+                    if(registrationModel.registration(0, txtFirstName.getText(), txtLastname.getText(), txtUsername.getText(), txtFirstPassword.getText(), tempRole, txtSecQuestion.getText(), txtSecAnswer.getText())) {
+                        dbStatus.setText("New account created");
+                } else {
+                    dbStatus.setText("Account failed to create");
+                }
+            } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+    }
+
+    private boolean checkPassword() {
+        if (txtFirstPassword.getText().equals(txtSecondPassword.getText())) {
+            passwordMatch.setText("Passwords match");
+            passwordMatch.setVisible(true);
+            return true;
+        } else {
+            passwordMatch.setText("Passwords do not match");
+            passwordMatch.setVisible(true);
+            return false;
+        }
+
+
+    }
 }
--- src/main/model/RegistrationModel.java
+++ src/main/model/RegistrationModel.java
@@ -1,4 +1,76 @@
 package main.model;
 
+import main.SQLConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class RegistrationModel {
+
+    Connection connection;
+
+    public RegistrationModel(){
+        connection= SQLConnection.connect();
+        if (connection == null)
+            System.exit(1);
+    }
+
+
+    //Checks if connection to DB is closed, if not, returns true, else false
+    public Boolean isDbConnected() {
+        try {
+            return !connection.isClosed();
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+
+    public Boolean registration(int employeeID, String firstName, String lastName, String username, String password, String role, String secQuestion, String secAnswer) throws SQLException {
+
+//        SQLConnection connect = new SQLConnection();
+//        Connection connection = connect.connect();
+
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        String query = "INSERT INTO employee(id, firstname, surname, username, password, role, secretquestion, secretquestionanswer, previousseat) VALUES (?, ?, ?, ?, ?, ?, ?, ?, NULL)";
+
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, employeeID);
+            preparedStatement.setString(2, firstName);
+            preparedStatement.setString(3, lastName);
+            preparedStatement.setString(4, username);
+            preparedStatement.setString(5, password);
+            preparedStatement.setString(6, role);           //TODO change role to be dynamic
+            preparedStatement.setString(7, secQuestion);
+            preparedStatement.setString(8, secAnswer);
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+
+                return true;
+            } else {
+                return false;
+            }
+        }
+        catch (Exception e)
+        {
+            return false;
+//        } finally {
+////            resultSet.close();
+////            preparedStatement.close();
+//            connection.close();
+//        }
+
+
+    }
+
+
+
+
 }
